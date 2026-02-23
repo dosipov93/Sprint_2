17a1f6040faa6a37392bafd3ae2d9b945b7e9823
Dmitriy <dmitrii0646@gmail.com>
2026-02-24 00:49:18 +0300

    style: исправить форматирование по PEP 8

--- Task_1.py
+++ Task_1.py
@@ -6,7 +6,7 @@ class Case:
         self.expected_result = expected_result
 
     def print_test_case_info(self):
-        print(f"ID тест-кейса:  {self.test_case_id}"
+        print(f"ID тест-кейса: {self.test_case_id}"
               f"\nНазвание: {self.name}"
               f"\nОписание шага: {self.step_description}"
               f"\nОжидаемый результат: {self.expected_result}")
@@ -14,19 +14,23 @@ class Case:
 # напиши свой код здесь
 
 class ExtendedCase(Case):
-    def __init__(self, test_case_id, name, step_description, expected_result,precondition,environment):
+    def __init__(self, test_case_id, name, step_description, expected_result, precondition, environment):
         super().__init__(test_case_id, name, step_description, expected_result)
         self.precondition = str(precondition)
         self.environment = str(environment)
 
-
     def print_test_case_info(self):
         super().print_test_case_info()
-        print (
+        print(
             f"\nПредусловие: {self.precondition}"
-            f"\nОкружение: {self.environment}"
-        )
+            f"\nОкружение: {self.environment}")
 
 
-case = ExtendedCase('1', 'Наличие кнопки Принять', '1. Открыть вкладку приёма документов 2. Проверить наличие кнопки ', 'Кнопка доступна', 'Открыть сервис', 'Яндекс Браузер')
+case = ExtendedCase('1',
+    'Наличие кнопки Принять',
+    '1. Открыть вкладку приёма документов 2. Проверить наличие кнопки',
+    'Кнопка доступна',
+    'Открыть сервис',
+    'Яндекс Браузер'
+)
 case.print_test_case_info()
