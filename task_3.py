dae9c23f8acb5d1cebc3d139793c4f8d77789637
Dmitriy <dmitrii0646@gmail.com>
2026-03-05 21:19:40 +0300

    style: исправить форматирование по PEP 8

--- task_3.py
+++ task_3.py
@@ -8,9 +8,7 @@ class PointsForPlace:
             print('Спортсмен не может занять нулевое или отрицательное место')
         else:
             points += 101 - place
-            
         return points
-
 
 class PointsForMeters:
     @staticmethod
@@ -20,8 +18,8 @@ class PointsForMeters:
             print('Количество метров не может быть отрицательным')
         else:
             points += meters *0.5
-            
         return points
+    
 class TotalPoints(PointsForPlace, PointsForMeters):
     def get_total_points(self, meters,place):
         total = self.get_points_for_place(place) + self.get_points_for_meters(meters)
