57ac6bc771007c3027b03e22d296d37ca856bb33
Dmitriy <dmitrii0646@gmail.com>
2026-03-05 21:16:54 +0300

    style: исправить форматирование по PEP 8

--- task_2.py
+++ task_2.py
@@ -1,23 +1,19 @@
 class Movies:
     def __init__(self):
         self.movies = []
-# напиши свой код здесь
 
     def add_movie(self,movie):
         self.movies.append(movie)
-
 
 class Comedy(Movies):
     def add_movie(self,movie):
         super().add_movie(movie)
         return f'Комедии: {self.movies}'
-    
 
 class Drama(Movies):
     def add_movie(self,movie):
         super().add_movie(movie)
         return f'Драмы: {self.movies}'
-
 
 comedy = Comedy()
 drama = Drama()
